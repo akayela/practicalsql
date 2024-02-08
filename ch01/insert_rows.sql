d78669dfd85d75ed8ed4ada55fcca4b968c107d5
akayela <akayela22@gmail.com>
2024-02-08 22:23:09 +0200

    insert rows

---
+++ ch01/insert_rows.sql
@@ -0,0 +1,7 @@
+insert into teachers (first_name, last_name, school, hire_date, salary)
+values ('Janet', 'Smith', 'F.D Roosevelt HS', '2011-10-30', 36200),
+('Lee', 'Reynolds', 'F.D Roosevelt HS', '1993-05-22', 65000),
+('Samuel', 'Cole', 'Myers Middle School', '2005-08-01', 43500),
+('Samantha', 'Bush', 'Myers Middle School', '2011-10-30', 36200),
+('Betty', 'Diaz', 'Myers Middle School', '2005-08-30', 43500),
+('Kathleen', 'Roush', 'F.D Roosevelt HS', '2010-10-22', 38500);
