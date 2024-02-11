dd94e018c6f45484555cb201732fb7ffed7ee6b3
akayela <akayela22@gmail.com>
2024-02-11 22:41:54 +0200

    Select statement

--- ch03/select_statement.sql
+++ ch03/select_statement.sql
@@ -24,3 +24,87 @@ select distinct school
 from teachers
 order by school;
 
+/* using distinct on multiple columns */
+select distinct school, salary
+from teachers
+order by school, salary;
+
+/* filtering rows with where */
+select last_name, school, hire_date
+from teachers
+where school = 'Myers Middle School';
+
+/* comparison and matching operators in PostgreSQL
+=        : where school = 'Myers Middle School',
+<> or != : where shcool != 'Baker Middle',
+>        : where salary > 2000,
+<        : where salary < 3000,
+>=       : where salary >= 3000,
+<=       : where salary <= 4000, 
+BETWEEN  : where salary BETWEEN 2000 And 4000 (means within a range),
+IN       : where last_name IN ('Bush', 'Roush') (match one of a set of values),
+LIKE     : where last_name LIKE 'Sam%' (match a pattern - case sensitive),
+ILIKE    : where last_name ILIKE 'sam%' (match a pattern - case insensitive),
+NOT      : where last_name NOT ILIKE 'sam%' (negates a condition)
+*/
+
+/* combining operators with AND and OR */
+select *
+from teachers
+where school = 'Myers Middle School'
+and salary < 40000;
+
+select * 
+from teachers
+where last_name = 'Cole'
+or last_name = 'Bush';
+
+select *
+from teachers
+where school ILIKE 'F.D%'
+and (salary < 38000 or salary > 40000);
+
+/* ch03 Try it yourself */
+select school, last_name
+from teachers
+order by school, last_name;
+
+select first_name
+from teachers
+where first_name Like 'S%'
+and salary > 40000;
+
+select *
+from teachers
+where hire_date >= '2010-01-01'
+order by salary desc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
